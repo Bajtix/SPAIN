ffdde80cfb966c5f8ef629f5a24fbc73dcbe1b03
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-02-05 16:49:22 +0100

    javascript progress 2

--- recipes.js
+++ recipes.js
@@ -8,7 +8,7 @@ function getText(ad)
       if (txtFile.status === 200 || txtFile.status === 0) {  // Makes sure it's found the file.
         allText = txtFile.responseText;
         text = allText;
-        alert(text);
+        //alert(text);
       }
     }
   }
@@ -18,5 +18,14 @@ function getText(ad)
   return sv;
 }
 
+
+const template = getText("recipe_template.html");
 const jsonFile = getText("recipes.json");
 const json = JSON.parse(jsonFile);
+let htmls = "";
+json.forEach((item, i) => {
+  name = item.name;
+  desc = item.description;
+  src = item.src;
+  htmls = htmls + template.replace("{name}",name).replace("{src}",src).replace("{desc}",desc);
+});
